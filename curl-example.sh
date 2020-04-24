036e59f51dc0fd9b49881f27c7f5c66ac1e9c1e9
Andreas Caternberg <cccaternberg@users.noreply.github.com>
2020-04-24 14:10:25 +0200

    Update curl-example.sh

--- curl-example.sh
+++ curl-example.sh
@@ -4,11 +4,12 @@
 APPLICATION_ID=5e95ad1fa6de037e1793732b
 USER_TOKEN=bff622c1-d6b4-4c52-a00a-b36455fbe929
 
+#list all environments
 curl  "https://x-api.rollout.io/public-api/applications/${APPLICATION_ID}/environments" \
   -H 'accept: application/json' \
   -H "Authorization: Bearer ${USER_TOKEN}" |jq 
 
-
+#list all experiemnts
 curl "https://x-api.rollout.io/public-api/applications/${APPLICATION_ID}/Production/experiments" \
   -H 'accept: application/json' \
   -H "Authorization: Bearer ${USER_TOKEN}" |jq
